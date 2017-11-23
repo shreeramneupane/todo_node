5310a51bfb51414323a1e4ff4549a8304cd0d5d0
shreeramneupane <shreeramneupane0@gmail.com>
2017-11-23 14:43:48 +0545

    DEV-3 Submit name to server

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
   res.end("Hello World!");
 });
 
 app.get('/hello_form', function(req, res) {
   res.sendFile(path.join(__dirname + '/views/hello_form.html'));
+});
+
+app.post('/hello', function (req, res) {
+  res.end(`Hello ${req.body.name}!`);
 });
 
 app.listen(port, () => {
--- routing.md
+++ routing.md
@@ -1,5 +1,6 @@
 # Routing and HTML page rendering
 
 * Let server know who you are
-  * Create a html page containing form with field **name**
-  
+  * Create an HTML page containing form with field **name**
+  * Create a new router which accepts **post** request. And, submit the from the route
+  * Use **body-parser** module to parse request
